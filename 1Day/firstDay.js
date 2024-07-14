f126c9ab6b287a1bcd68505a54d224c90a158499
saurabhack <kamanesaurabh68@gmail.com>
2024-07-14 21:32:47 +0530

    the challenge of first and second days challenge

---
+++ 1Day/firstDay.js
@@ -0,0 +1,43 @@
+//variable declaration
+var num=10
+console.log(num)
+
+let str="saurabh"
+console.log(str)
+
+//constant Declaration
+
+const first=true
+console.log(first)
+
+//Data types
+
+let number1=10
+console.log(typeof number1)
+let name="saurabh vikas kamane"
+console.log(typeof name)
+let flag=false
+console.log(typeof flag)
+
+let obj={
+    name:"saurabh vikas kamane",
+    age:21,
+    number:9881910567
+}
+console.log(typeof obj)
+
+let arr=new Array(1,2,3,4)
+console.log(typeof arr)
+
+//reassinging values
+let fval=10
+console.log(fval)
+fval=20
+console.log(fval)
+
+//Understanding consts
+
+const sval=10
+console.log(sval)
+sval=20
+console.log(sval)
---
+++ 2Day/secondDay.js
@@ -0,0 +1,31 @@
+let reminder=10%2
+console.log(reminder)
+
+//asignment operators
+
+let first=1;
+let second=2;
+second+=first
+console.log(second)
+second-=first
+console.log(second)
+
+//comparison operator
+console.log(10<12)
+console.log(10>12)
+
+console.log(10<=20)
+console.log(20>=10)
+
+console.log(10===20)
+console.log(10==10)
+
+//logical operators
+let age=18
+console.log(age>=18 && age<=20)
+console.log(age<=18 || age>=20)
+console.log(age!=20)
+
+//ternary operators
+let num=20
+num%2==0 ? console.log("even") : console.log("odd")
